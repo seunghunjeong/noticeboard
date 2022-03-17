e0775731662639b95059b0c166620576b9417bdb
jung seung hoon <todtkddj@naver.com>
2022-03-17 12:34:24 +0900

    layout components 분리 / 글 입력 navigate 경로 변경

--- client/src/components/BoardingPage/Board_register.js
+++ client/src/components/BoardingPage/Board_register.js
@@ -1,8 +1,73 @@
 import React from 'react'
+import Axios from 'axios';
+import { useState } from 'react';
+import { useNavigate  } from 'react-router-dom';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import Editor from '@ckeditor/ckeditor5-build-classic';
 
 function Board_register() {
+
+  const asd = Editor.defaultConfig;
+
+  console.log(asd);
+  const [boardContent, setBoardContent] = useState({
+    title: '',
+    content: ''
+  })
+
+  // 에디터에서 입력값 받아오는 함수
+  const getValue = e => {
+    const {name, value} = e.target;
+    setBoardContent({
+      ...boardContent,
+      [name]: value
+    })
+  }
+
+  
+  // 이벤트 후 경로 이동할때 사용하는 hooks
+  const navigate = useNavigate();  
+  // 입력버튼
+  const submitBoard = () => {
+    const title = boardContent.title;
+    const content = boardContent.content;
+   
+    
+
+    if(title === ""){
+      alert('제목을 입력해주세요.');
+      return;
+    }
+
+    Axios.post('http://localhost:8000/api/insert', {
+      title: title,
+      content: content
+    }).then(()=>{
+      navigate('/board_list');
+      alert('등록완료');
+    })
+  };
+
   return (
-    <div>Board_register</div>
+    <div>
+      <div className='form-wrapper'>
+        <input type='text' placeholder='제목' onChange={getValue} name='title' />
+        <CKEditor
+          editor={Editor}
+          data=""
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            setBoardContent({
+              ...boardContent,
+              content: data
+            })
+          }}
+        />
+      <button variant="contained"
+        onClick={submitBoard}
+      >입력</button>
+      </div>
+    </div>
   )
 }
 
