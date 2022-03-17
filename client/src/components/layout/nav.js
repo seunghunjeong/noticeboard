d086d6a0c0c03c94ec9707c81b11fa358565e5ad
ga-youn Park <gyym9310@gmail.com>
2022-03-17 18:34:20 +0900

    nav바 링크연결

--- client/src/components/layout/nav.js
+++ client/src/components/layout/nav.js
@@ -1,33 +1,40 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Button } from 'antd';
 
 import { HomeOutlined, ProfileOutlined, SettingOutlined, TeamOutlined } from '@ant-design/icons';
 
 import { Link } from 'react-router-dom';
 
+//antd 
 const { Sider } = Layout;
-
 const { SubMenu } = Menu;
 
+
+
 function nav() {
+
     return (
       <Sider>
-            <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<HomeOutlined />}>
-                  Home
-                </Menu.Item>
-                <Menu.Item key="2" icon={<ProfileOutlined />}>
-                  Board
-                </Menu.Item>
-                <SubMenu key="sub" icon={<TeamOutlined />} title="Team">
-                  <Menu.Item key="3">Team 1</Menu.Item>
-                  <Menu.Item key="4">Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="5" icon={<SettingOutlined />}>
-                  Setting
-                </Menu.Item>
-            </Menu>
-          </Sider>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1" icon={<HomeOutlined />}>
+                <Link to={'/'}>Home</Link>
+              </Menu.Item>
+              {/* <Menu.Item key="2" icon={<TeamOutlined />}>
+                Team
+              </Menu.Item> */}
+              <SubMenu key="sub" icon={<ProfileOutlined />} title="Board">
+                <Menu.Item key="3">notice</Menu.Item>
+                <Menu.Item key="4"><Link to={'/board_list/'}>project</Link></Menu.Item>
+              </SubMenu>
+              {/* <Menu.Item key="5" icon={<SettingOutlined />}>
+                Setting
+              </Menu.Item> */}
+          </Menu>
+          <div style={{ width : '100%', textAlign : 'center', position : 'absolute', bottom : '10px', left : '0' }}>
+            <Button type="primary" danger style={{ width : '90%' }}>로그인</Button>
+            <Button type="primary" danger style={{ width : '90%' }}>로그아웃</Button>
+          </div>
+       </Sider>
     )
 }
 
