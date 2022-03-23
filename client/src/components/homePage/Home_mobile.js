2e5781cfc4f2ceb5d036ce23d13984c5d880f6db
jung seung hoon <todtkddj@naver.com>
2022-03-23 18:17:55 +0900

    모바일 등록기능 추가

--- client/src/components/homePage/Home_mobile.js
+++ client/src/components/homePage/Home_mobile.js
@@ -1,10 +1,8 @@
 import React, { Fragment, useState } from 'react'
 import Axios from 'axios';
 import 'antd/dist/antd.less';
-import { Card, Layout, Calendar, Badge, Button, Tag, Tabs } from 'antd';
+import { Calendar, Badge, Tag } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import { useNavigate, Link, useParams } from "react-router-dom"
-import ReactHtmlParser from 'react-html-parser';
 import locale from "antd/es/calendar/locale/ko_KR";
 import Modal from './DailyReportPopup';
 import TextArea from 'antd/lib/input/TextArea';
@@ -13,24 +11,31 @@ import moment from 'moment';
 import MobileStyle from '../../App_mobile.module.css';
 
 function Home() {
+    
+    // dailyReport 정보
+    const [dailyReport, setDailyReport] = useState(
+        {
+            id : 'test',
+            writer : '임시작성자',
+            report : '',
+            regist_date : ''
+        }
+    );
 
     // 월 단위 캘린더 랜더링할 내용
     function getListData(value) {
         let listData;
         let i = 1
-        let content = "test";
         let type = "success";
-        let index = [1, 2];
-        // switch (value.date()) {
-        //     case i:
-        //         listData = [
-        //             { index: index[0], type: type, content: content },
-        //             { index: index[1], type: type, content: content }
-        //         ];
-        //         break;
+        switch (value.date()) {
+            case i:
+                listData = [
+                    { type: type }
+                ];
+                break;
 
-        //     default:
-        // }
+            default:
+        }
         return listData || [];
     }
 
@@ -89,8 +94,30 @@ function Home() {
         setSelectDay({
             selectedValue: value,
         });
+
+        // 등록일 저장
+        setDailyReport({
+            ...dailyReport,
+            regist_date : value.format('YYYY-MM-DD')
+        })
     };
 
+    // 일보 내용 저장
+    const textAreaHandleChange = (event) => {
+        setDailyReport({
+            ...dailyReport,
+            report : event.target.value
+        })
+    }
+
+    // 일보 저장
+    const insertBogo = () => {
+        console.log(dailyReport);
+        Axios.post('http://localhost:8000/api/insertR', dailyReport
+        ).then(() => {
+        alert('일일보고가 작성되었습니다.');
+        })
+    }
 
     return (
         <Fragment>
@@ -105,10 +132,10 @@ function Home() {
                 // monthCellRender={monthCellRender}
                 onSelect={onSelect}
             />
-            <Modal open={modalOpen} close={closeModal} header="일일 보고" insert={() => { alert("click"); }}>
+            <Modal open={modalOpen} close={closeModal} header="일일 보고" insert={insertBogo}>
                 <Tag style={{ marginBottom: '5px' }}>작성자 :이름</Tag>
                 <Tag style={{ marginBottom: '5px' }}>작성일 :{selectDay.selectedValue.format('YYYY-MM-DD')}</Tag>
-                <TextArea style={{ height: '300px' }}></TextArea>
+                <TextArea style={{ height: '300px' }} onChange={textAreaHandleChange}></TextArea>
             </Modal>
         </Fragment>
     )
