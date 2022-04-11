4051c8fe17d8678cf2902253d1730423be45adc8
Kim kyung bin <rudqls001@naver.com>
2022-04-11 16:25:59 +0900

    부서 , 직급 부여기능 추가 / 유저 삭제기능 추가 / 관리자 지정기능 가입승인 -> 유저관리 탭으로 이동

--- server/routers/loginRouter.js
+++ server/routers/loginRouter.js
@@ -15,11 +15,11 @@ router.post('/standby-signup', (req, res, next) => {
 
   const id = req.body.id;
   const password = req.body.password;
-  const username = req.body.username; 
+  const username = req.body.username;
   const sqlQuery = "INSERT INTO board.users (id, username, password) VALUES (?, ?, ?)"
-  
+
   // 비밀번호 > 암호화시켜서 insert 
-  bcrypt.genSalt(saltRounds, function(err, salt) {
+  bcrypt.genSalt(saltRounds, function (err, salt) {
     bcrypt.hash(password, salt, (err, hash) => {
       db.query(sqlQuery, [id, username, hash], (err, result) => {
         if (err) {
@@ -30,24 +30,38 @@ router.post('/standby-signup', (req, res, next) => {
           })
         }
         return res.json({
-          msg : "success"
+          msg: "success"
         });
       });
-    });      
+    });
   });
 });
 
 //회원가입신청 목록 불러오기
 router.get("/getStandby_signup", (req, res) => {
-    
+
   //const sqlQuery = `SELECT * FROM board.users WHERE status  = 'N'`;
-  const sqlQuery = `SELECT * FROM board.users ORDER BY registered DESC, status ASC`;
+  const sqlQuery = `
+    SELECT 
+	   id,
+	   username,
+	   registered,
+	   status,
+	   approved,
+	   auth,
+	   department,
+	   position
+    FROM
+     board.users
+    ORDER BY 
+     registered DESC, status ASC
+  `;
 
   db.query(sqlQuery, (err, result) => {
-      if (err) {
-        logger.error(err);
-      }
-      res.send(result);
+    if (err) {
+      logger.error(err);
+    }
+    res.send(result);
   });
 
 })
@@ -57,19 +71,19 @@ router.post('/approve-sign-up', (req, res, next) => {
 
   const id = req.body.id;
   const sqlQuery = "UPDATE board.users SET status = 'Y', approved = CURRENT_TIMESTAMP WHERE id = ?"
-  
+
   db.query(sqlQuery, [id], (err, result) => {
-      if (err) {
-        //throw err;
-        logger.error(err);
-        return res.json({
-          msg: "가입승인오류. 관리자에게 문의해주세요.",
-        })
-      }
+    if (err) {
+      //throw err;
+      logger.error(err);
       return res.json({
-        msg : "success"
-      });
+        msg: "가입승인오류. 관리자에게 문의해주세요.",
+      })
     }
+    return res.json({
+      msg: "success"
+    });
+  }
   );
 });
 
@@ -78,19 +92,19 @@ router.post('/reject-sign-up', (req, res, next) => {
 
   const id = req.body.id;
   const sqlQuery = "DELETE FROM board.users WHERE id = ?"
-  
+
   db.query(sqlQuery, [id], (err, result) => {
-      if (err) {
-        //throw err;
-        logger.error(err);
-        return res.json({
-          msg: "가입거절오류. 관리자에게 문의해주세요.",
-        })
-      }
+    if (err) {
+      //throw err;
+      logger.error(err);
       return res.json({
-        msg : "success"
-      });
+        msg: "가입거절오류. 관리자에게 문의해주세요.",
+      })
     }
+    return res.json({
+      msg: "success"
+    });
+  }
   );
 });
 
@@ -99,19 +113,19 @@ router.post('/admin-appoint', (req, res, next) => {
 
   const id = req.body.id;
   const sqlQuery = "UPDATE board.users SET auth = 1 WHERE id = ?"
-  
+
   db.query(sqlQuery, [id], (err, result) => {
-      if (err) {
-        //throw err;
-        logger.error(err);
-        return res.json({
-          msg: "관리자 지정 오류. 관리자에게 문의해주세요.",
-        })
-      }
+    if (err) {
+      //throw err;
+      logger.error(err);
       return res.json({
-        msg : "success"
-      });
+        msg: "관리자 지정 오류. 관리자에게 문의해주세요.",
+      })
     }
+    return res.json({
+      msg: "success"
+    });
+  }
   );
 });
 
@@ -120,19 +134,19 @@ router.post('/admin-remove', (req, res, next) => {
 
   const id = req.body.id;
   const sqlQuery = "UPDATE board.users SET auth = null WHERE id = ?"
-  
+
   db.query(sqlQuery, [id], (err, result) => {
-      if (err) {
-        //throw err;
-        logger.error(err);
-        return res.json({
-          msg: "관리자 해지 오류. 관리자에게 문의해주세요.",
-        })
-      }
+    if (err) {
+      //throw err;
+      logger.error(err);
       return res.json({
-        msg : "success"
-      });
+        msg: "관리자 해지 오류. 관리자에게 문의해주세요.",
+      })
     }
+    return res.json({
+      msg: "success"
+    });
+  }
   );
 });
 
@@ -143,88 +157,88 @@ router.post('/login', (req, res, next) => {
   const id = req.body.id;
   const password = req.body.password;
   const sqlLogin = 'SELECT * FROM board.users WHERE BINARY(id) = ?'
-  
+
   db.query(sqlLogin, [id], (err, result) => {
-      // 유저 확인 x
-      if (err) {
+    // 유저 확인 x
+    if (err) {
+      logger.error(err);
+      return res.json({
+        msg: err,
+        loginSuccess: false
+      });
+    }
+
+    if (!result.length) {
+      return res.json({
+        msg: '아이디가 존재하지않습니다.',
+        loginSuccess: false
+      });
+
+    }
+    // 유저 확인 o
+    if (result[0].status === "N") {
+      return res.json({
+        msg: '가입승인 대기중입니다.',
+        loginSuccess: false
+      });
+    }
+    else {
+      //암호화된 비밀번호를 바로 복호화시킬순 없기때문에
+      //원래의 비밀번호를 암호화시킨후 맞는지 확인해야한다.
+      bcrypt.compare(password, result[0]['password'], (bErr, bResult) => {
+        // wrong password
+        if (bErr) {
+          logger.error(bErr);
+          return res.json({
+            msg: "패스워드가 틀렸습니다.",
+            loginSuccess: false
+          });
+        }
+        //아이디와 패스워드가 같아서 결과값이 나왔다면
+        //토큰생성
+        if (bResult) {
+          // jwt
+          // const token = jwt.sign( {token : result[0].id},
+          //   'SECRETKEY', { expiresIn : '86400' });//만료기간24 hours
+
+          //토큰을 저장한다. where ? 쿠키 or 로컬스토리지
+          //쿠키에 저장할 것-> cookie-parser 라이브러리 설치
+          // res.cookie("test", "test");
+          // res.cookie("accessToken", token, {
+          //   maxAge: 10000, 
+          //   expires  : new Date(Date.now() + 9999999), 
+          //   httpOnly :false
+          // });
+          // 로그인 후 사용자 정보를 세션에 저장 
+          // req.session.isLogin = result[0].id
+          // req.session.userName = result[0].username
+          // req.session.save(error => {
+          //   if(error) console.log(error)
+          // })
+
+          return res.json({
+            msg: '로그인 성공',
+            //accessToken: token,
+            userId: result[0].id,
+            loginSuccess: true
+          });
+        }
         logger.error(err);
         return res.json({
-          msg: err,
-          loginSuccess : false
+          msg: '아이디나 패스워드가 틀렸습니다.',
+          loginSuccess: false
         });
       }
-
-      if (!result.length) {
-        return res.json({
-          msg: '아이디가 존재하지않습니다.',
-          loginSuccess : false
-        });
-        
-      }
-      // 유저 확인 o
-      if(result[0].status === "N"){
-        return res.json({
-          msg: '가입승인 대기중입니다.',
-          loginSuccess : false
-        });
-      } 
-      else {
-          //암호화된 비밀번호를 바로 복호화시킬순 없기때문에
-          //원래의 비밀번호를 암호화시킨후 맞는지 확인해야한다.
-          bcrypt.compare(password, result[0]['password'], (bErr, bResult) => {
-            // wrong password
-            if (bErr) {
-              logger.error(bErr);
-              return res.json({
-                msg: "패스워드가 틀렸습니다.",
-                loginSuccess : false
-              });
-            }
-            //아이디와 패스워드가 같아서 결과값이 나왔다면
-            //토큰생성
-            if (bResult) {
-              // jwt
-              // const token = jwt.sign( {token : result[0].id},
-              //   'SECRETKEY', { expiresIn : '86400' });//만료기간24 hours
-
-              //토큰을 저장한다. where ? 쿠키 or 로컬스토리지
-              //쿠키에 저장할 것-> cookie-parser 라이브러리 설치
-              // res.cookie("test", "test");
-              // res.cookie("accessToken", token, {
-              //   maxAge: 10000, 
-              //   expires  : new Date(Date.now() + 9999999), 
-              //   httpOnly :false
-              // });
-              // 로그인 후 사용자 정보를 세션에 저장 
-              // req.session.isLogin = result[0].id
-              // req.session.userName = result[0].username
-              // req.session.save(error => {
-              //   if(error) console.log(error)
-              // })
-                
-              return res.json({
-                msg: '로그인 성공',
-                //accessToken: token,
-                userId : result[0].id,
-                loginSuccess : true
-              });
-            }
-            logger.error(err);
-            return res.json({
-              msg: '아이디나 패스워드가 틀렸습니다.',
-              loginSuccess : false
-            });
-          }
-        ); 
-      }
+      );
     }
+  }
   );
 });
 
 
 //페이지로딩시 로그인 유무 권한 체크
 router.post('/auth', (req, res, next) => {
-  
+
   const userId = req.body.userId;
   const sqlLogin = 'SELECT * FROM board.users WHERE id = ? AND status = "Y"'
 
@@ -233,48 +247,48 @@ router.post('/auth', (req, res, next) => {
     if (err) {
       logger.error(err);
       return res.json({
-        userName : null,
-        id : null,
-        isAuth : false,
-        admin : false
+        userName: null,
+        id: null,
+        isAuth: false,
+        admin: false
       });
     }
     // 인증실패
     if (!result.length) {
       return res.json({
-        userName : null,
-        id : null,
-        isAuth : false,
-        admin : false
+        userName: null,
+        id: null,
+        isAuth: false,
+        admin: false
       });
     }
     // 인증성공
     else {
       return res.json({
-        userName : result[0].username,
-        id : result[0].id,
-        isAuth : true,
-        admin : result[0].auth === 1 ? true : false
-    });
+        userName: result[0].username,
+        id: result[0].id,
+        isAuth: true,
+        admin: result[0].auth === 1 ? true : false
+      });
     }
   });
- 
+
 })
 
 //로그아웃
 router.get('/logout', (req, res) => {
-  
-  try{   
-    return res.json({  
-      isAuth : false,
-      id : null,
-      userName : null,
-      logoutSuccess : true
+
+  try {
+    return res.json({
+      isAuth: false,
+      id: null,
+      userName: null,
+      logoutSuccess: true
     })
   }
-  catch(err) {
+  catch (err) {
     logger.error(err);
-    res.json({ logoutSuccess : err})
+    res.json({ logoutSuccess: err })
   }
 })
 
@@ -282,14 +296,14 @@ router.get('/logout', (req, res) => {
 // isLoggedIn : (req, res, next) => {    
 
 //   try {
-   
+
 //     //token decoded
 //     const token = req.headers.authorization.split('Bearer ')[1];
 //     //console.log(token)
 //     const decoded = jwt.verify(token, 'SECRETKEY');
 //     //console.log(decoded.token)
 //     const sqlLogin = 'SELECT * FROM board.users WHERE id = ?'
-    
+
 //     db.query(sqlLogin, [decoded.token], (err, result) => {
 //       // 인증실패
 //       if (err) {
