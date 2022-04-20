410a9e64a36a4355f243a893006ad9ed7e0fe095
jung seung hoon <todtkddj@naver.com>
2022-04-20 13:57:06 +0900

    모바일 화면 nav 개선

--- client/src/components/layout/Header_mobile.js
+++ client/src/components/layout/Header_mobile.js
@@ -10,7 +10,7 @@ import Auth from '../../_hoc/auth'
 import NavLayout from './Nav';
 
 import { Layout, Avatar, Drawer } from 'antd';
-import {BarsOutlined } from '@ant-design/icons';
+import { MenuOutlined, FileDoneOutlined } from '@ant-design/icons';
 import MobileStyle from '../../App_mobile.module.css';
 
 
@@ -47,7 +47,7 @@ function header(props) {
     
         return(
         <>
-            <BarsOutlined onClick={showDrawer} /> 
+            <MenuOutlined onClick={showDrawer} /> 
             <Drawer placement="right" onClose={navClose} visible={visible} closable={false} width={'70vw'} bodyStyle={{padding:0, backgroundColor: 'rgb(0, 21, 41)'}}>
                 <NavLayout props={boardCategory} admin={admin} state={ss}> </NavLayout>
             </Drawer>
