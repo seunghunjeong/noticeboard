c7fcbf6e6d768c91af21bb05033fa6d6779012d1
jung seung hoon <todtkddj@naver.com>
2022-06-09 18:01:36 +0900

    footer 수정

--- client/src/components/layout/footer.js
+++ client/src/components/layout/footer.js
@@ -4,8 +4,7 @@ const { Footer } = Layout;
 
 function footer() {
     return (
-        <Footer style={{ textAlign: 'center', fontSize : "11px" }}> 08378 서울특별시 구로구 디지털로 306, 608호
-        Tel : 02-578-5101 | Fax : 02-578-5191 ©2022. CmWorld INC all right reserved.</Footer>
+        <Footer style={{ textAlign: 'center' }}> 게시판 이름 미정ㅎ.ㅎ ©2022 Created by https://github.com/seunghunjeong/noticeboard</Footer>
     )
 }
 
