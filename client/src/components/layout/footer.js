67a02c832c379c81f64dbbd39b5859f58095b46f
Kim kyung bin <rudqls001@naver.com>
2022-11-11 11:39:52 +0900

    주소 수정

--- client/src/components/layout/footer.js
+++ client/src/components/layout/footer.js
@@ -4,7 +4,7 @@ const { Footer } = Layout;
 
 function footer() {
     return (
-        <Footer style={{ textAlign: 'center', fontSize : "11px" }}> 08505 서울시 금천구 가산디지털로2로 101, 1705호
+        <Footer style={{ textAlign: 'center', fontSize : "11px" }}> 08505 서울시 금천구 가산디지털로2로 101, A동 1705호
         Tel : 02-578-5101 | Fax : 02-578-5191 ©2022. CmWorld INC all right reserved.</Footer>
     )
 }
